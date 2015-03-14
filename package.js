93c2c46b8facc566a410e0dc6ea9b285123b9c64
森田 昌宏 <morita@gamba-morita-macbook-air.local>
2015-03-14 13:03:53 +0900

    modify package.js

--- package.js
+++ package.js
@@ -2,16 +2,16 @@ Package.describe({
   name: 'mmorita:bootcardsjs',
   version: '1.1.0',
   // Brief, one-line summary of the package.
-  summary: 'bootcards.js - A cards-based UI framework for mobile and desktop apps, built on top of Bootstrap',
+  summary: 'bootcards.js for Meteor - A cards-based UI framework for mobile and desktop apps, built on top of Bootstrap',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/MasahiroMorita/mmorita-bootcardsjs',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.0');
+  api.versionsFrom('0.9.0');
   api.use('twbs:bootstrap', 'client');
   api.addFiles('bootcards-1.1.0/bootcardsjs.js', 'client');
   api.addFiles('bootcards-1.1.0/dist/css/bootcards-desktop-lite.min.css', 'web.browser');
