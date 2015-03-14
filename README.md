6235991d4cc0a7935a6f35c2ed703afc52029319
森田 昌宏 <morita@gamba-morita-macbook-air.local>
2015-03-14 15:39:19 +0900

    write README

---
+++ README.md
@@ -0,0 +1,17 @@
+# bootcards.js
+
+[Bootcards.js](http://www.bootcards.com) pakaged for Meteor.js.
+
+Bootcards.js is a cards-based UI with dual-pane capability for mobile and desktop, built on top of Bootstrap.
+
+# Installation
+
+```
+meteor add mmorita:bootcardsjs
+``
+
+# Dependencies
+
+* twbs:bootstrap
+
+
--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'mmorita:bootcardsjs',
   version: '1.1.0',
   // Brief, one-line summary of the package.
-  summary: 'bootcards.js for Meteor - A cards-based UI framework for mobile and desktop apps, built on top of Bootstrap',
+  summary: 'bootcards.js for Meteor - A cards-based UI framework for mobile and desktop apps',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/MasahiroMorita/mmorita-bootcardsjs',
   // By default, Meteor will default to using README.md for documentation.
@@ -12,8 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('0.9.0');
-  api.use('twbs:bootstrap', 'client');
-  api.addFiles('bootcards-1.1.0/bootcardsjs.js', 'client');
+  api.use('twbs:bootstrap@3.3.2', 'client');
   api.addFiles('bootcards-1.1.0/dist/css/bootcards-desktop-lite.min.css', 'web.browser');
   api.addFiles('bootcards-1.1.0/dist/css/bootcards-ios-lite.min.css', 'web.cordova');
 
@@ -22,7 +21,6 @@ Package.onUse(function(api) {
   api.addFiles('bootcards-1.1.0/dist/fonts/icomoon.ttf', 'client');
   api.addFiles('bootcards-1.1.0/dist/fonts/icomoon.woff', 'client');
 
-  api.addFiles('bootcards-1.1.0/dist/js/bootcards.js', 'client');
   api.addFiles('bootcards-1.1.0/dist/js/bootcards.min.js', 'client');
 });
 
